b24a6ef586704104a0b9c0f0d2440ba7c1881e23
MohammadHosseini <sayedmh50@gmail.com>
2022-07-26 12:33:54 +0430

    write vmallocationpolicyabstract

--- src/main/java/org/cloudbus/cloudsim/gp/allocationpolicies/CustomVmAllocationPolicyAbstract.java
+++ src/main/java/org/cloudbus/cloudsim/gp/allocationpolicies/CustomVmAllocationPolicyAbstract.java
@@ -1,1 +1,9 @@
+package org.cloudbus.cloudsim.gp.allocationpolicies;
 
+import org.cloudbus.cloudsim.allocationpolicies.VmAllocationPolicy;
+
+public abstract class CustomVmAllocationPolicyAbstract implements VmAllocationPolicy{
+	
+	
+	
+}
