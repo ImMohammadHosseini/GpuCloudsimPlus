c1f77aa244ce237d29d5398b013810f343b9be6e
MohammadHosseini <sayedmh50@gmail.com>
2022-07-27 12:14:30 +0430

    change vgpus file

--- src/main/java/org/cloudbus/cloudsim/gp/vms/vgpus/CustomVGpu.java
+++ src/main/java/org/cloudbus/cloudsim/gp/vms/vgpus/CustomVGpu.java
@@ -1,4 +1,13 @@
-package org.cloudbus.cloudsim.gp.vgpus;
+package org.cloudbus.cloudsim.gp.vms.vgpus;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public interface CustomVGpu {
+	
+    Logger LOGGER = LoggerFactory.getLogger(Vm.class.getSimpleName());
+
+	
+}
 
