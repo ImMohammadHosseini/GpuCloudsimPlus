1abc8638a000978d786c63dca26b7fb52328aae2
MohammadHosseini <sayedmh50@gmail.com>
2022-08-02 09:08:47 +0430

    change in gpu classes

--- src/main/java/org/cloudbus/cloudsim/gp/resources/Gpu.java
+++ src/main/java/org/cloudbus/cloudsim/gp/resources/Gpu.java
@@ -1,1 +1,15 @@
 
+package org.cloudbus.cloudsim.gp.resources;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.cloudbus.cloudsim.vms.Vm;
+
+
+public interface Gpu {
+	
+    Logger LOGGER = LoggerFactory.getLogger(Vm.class.getSimpleName());
+    
+}
