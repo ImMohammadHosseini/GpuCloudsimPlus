cbbde2689d3122ee81cd391644fcb5c97f482b21
MohammadHosseini <sayedmh50@gmail.com>
2022-08-04 12:48:57 +0430

    change VGpuStateHistoryEntry

--- src/main/java/org/cloudbus/cloudsim/gp/resources/VGpuStateHistoryEntry.java
+++ src/main/java/org/cloudbus/cloudsim/gp/resources/VGpuStateHistoryEntry.java
@@ -1,6 +1,11 @@
 package org.cloudbus.cloudsim.gp.resources;
 
+import org.cloudbus.cloudsim.vms.VmStateHistoryEntry;
 
-public class VGpuStateHistoryEntry {
+public class VGpuStateHistoryEntry extends VmStateHistoryEntry {
 	
+	public VGpuStateHistoryEntry (final double time, final double allocatedMips, 
+			final double requestedMips, final boolean inMigration) {
+		super(time, allocatedMips, requestedMips, inMigration);
+	}
 }
