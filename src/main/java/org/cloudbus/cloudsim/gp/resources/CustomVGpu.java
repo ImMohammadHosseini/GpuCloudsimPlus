641b2c56f50e31656b4219deb3a93acc274513a8
MohammadHosseini <sayedmh50@gmail.com>
2022-08-04 12:28:33 +0430

    add attributes in customVGpu

--- src/main/java/org/cloudbus/cloudsim/gp/resources/CustomVGpu.java
+++ src/main/java/org/cloudbus/cloudsim/gp/resources/CustomVGpu.java
@@ -1,7 +1,21 @@
 package org.cloudbus.cloudsim.gp.resources;
 
+import org.cloudbus.cloudsim.gp.vms.CustomGpuVmSimple;
+import org.cloudbus.cloudsim.resources.Ram;
+import org.cloudbus.cloudsim.resources.Bandwidth;
+import org.cloudbus.cloudsim.resources.Processor;
 
 public class CustomVGpu {
+	
+	private long id;
+	private String type;
+	
+	private CustomGpuVmSimple gpuVm;
+	
+	private Processor vGpuProcessors;
+	private Ram gddram;
+	private Bandwidth bw;
+	
 	
 	public CustomVGpu () {
 		
--- src/main/java/org/cloudbus/cloudsim/gp/vms/CustomGpuVmSimple.java
+++ src/main/java/org/cloudbus/cloudsim/gp/vms/CustomGpuVmSimple.java
@@ -1,1 +1,8 @@
+package org.cloudbus.cloudsim.gp.vms;
 
+import org.cloudbus.cloudsim.vms.Vm;
+import org.cloudbus.cloudsim.core.CustomerEntityAbstract;
+
+public class CustomGpuVmSimple extends CustomerEntityAbstract implements Vm {
+	
+}
