77211a941fb80b553d0e736e7711cbff0998c40d
MohammadHosseini <sayedmh50@gmail.com>
2022-08-06 12:24:17 +0430

    program gpuTask and GpuTaskSimple attributes

--- src/main/java/org/cloudbus/cloudsim/gp/cloudlets/gputasks/GpuTask.java
+++ src/main/java/org/cloudbus/cloudsim/gp/cloudlets/gputasks/GpuTask.java
@@ -2,6 +2,13 @@ package org.cloudbus.cloudsim.gp.cloudlets.gputasks;
 
 import org.cloudbus.cloudsim.gp.cloudlets.GpuCloudletSimple;
 
+import org.cloudbus.cloudsim.utilizationmodels.UtilizationModel;
+import org.cloudbus.cloudsim.resources.ResourceManageable;
+import org.cloudsimplus.listeners.CloudletVmEventInfo;
+import org.cloudsimplus.listeners.EventListener;
+
+import java.util.List;
+
 public interface GpuTask {
 	
 	enum Status {
@@ -21,4 +28,131 @@ public interface GpuTask {
 	GpuCloudletSimple getGpuCloudlet ();
 	
 	void setGpuCloudlet (GpuCloudletSimple GpuCloudlet);
+	
+	boolean addRequiredFile(String fileName);
+
+    boolean addRequiredFiles(List<String> fileNames);
+
+    boolean deleteRequiredFile(String filename);
+
+    boolean hasRequiresFiles();
+
+    List<String> getRequiredFiles();
+
+    double getArrivalTime();
+    
+    double getActualGpuTime();
+
+    long getFileSize();
+
+    long getOutputSize();
+
+    Status getStatus();
+
+    //@Override
+    //double getSubmissionDelay();
+
+    //boolean isReturnedToBroker();
+
+    //double registerArrivalInDatacenter();
+    
+    double getExecStartTime();
+
+    double getFinishTime();
+
+    long getTaskId();
+
+    void setTaskId(long taskId);
+    
+    int getPriority();
+    
+    GpuTask setPriority(int priority);
+
+    //int getNetServiceLevel();
+
+    long getNumberOfPes();
+
+    UtilizationModel getUtilizationModelBw();
+    
+    UtilizationModel getUtilizationModelGpu();
+
+    UtilizationModel getUtilizationModelGddram();
+
+    UtilizationModel getUtilizationModel(Class<? extends ResourceManageable> resourceClass);
+    
+    //double getUtilizationOfGpu();
+
+    double getUtilizationOfGpu(double time);
+
+    double getUtilizationOfGddram();
+    
+    double getUtilizationOfGddram(double time);
+
+    double getUtilizationOfBw();
+
+    double getUtilizationOfBw(double time);
+    
+    boolean isBoundToVm();
+
+    //double getWaitingTime();
+
+    boolean isFinished();
+
+    GpuTask setFileSize(long fileSize);
+
+    GpuTask setOutputSize(long outputSize);
+    
+    GpuTask setSizes(long size);
+
+    boolean setStatus(Status newStatus);
+    
+    //void setNetServiceLevel(int netServiceLevel);
+
+    GpuTask setNumberOfPes(long numberOfPes);
+    
+    GpuTask setUtilizationModel(UtilizationModel utilizationModel);
+
+    GpuTask setUtilizationModelBw(UtilizationModel utilizationModelBw);
+
+    GpuTask setUtilizationModelGpu(UtilizationModel utilizationModelGpu);
+
+    GpuTask setUtilizationModelGddram(UtilizationModel utilizationModelGddram);
+
+    long getBlockLength();
+    
+    GpuTask setBlockLength(long length);
+
+    //long getTotalLength();
+    
+    long getFinishedLengthSoFar();
+
+    boolean addFinishedLengthSoFar(long partialFinishedMI);
+
+    void setExecStartTime(double clockTime);
+    
+    GpuTask addOnStartListener(EventListener<CloudletVmEventInfo> listener);
+
+    boolean removeOnStartListener(EventListener<CloudletVmEventInfo> listener);
+
+    GpuTask addOnUpdateProcessingListener(EventListener<CloudletVmEventInfo> listener);
+
+    boolean removeOnUpdateProcessingListener(EventListener<CloudletVmEventInfo> listener);
+    
+    GpuTask addOnFinishListener(EventListener<CloudletVmEventInfo> listener);
+
+    boolean removeOnFinishListener(EventListener<CloudletVmEventInfo> listener);
+
+    void notifyOnUpdateProcessingListeners(double time);
+
+    //@Override
+    //DatacenterBroker getBroker();
+
+    //@Override
+    //void setBroker(DatacenterBroker broker);
+
+    GpuTask reset();
+
+    GpuTask setLifeTime(double lifeTime);
+
+	double getLifeTime();
 }
--- src/main/java/org/cloudbus/cloudsim/gp/cloudlets/gputasks/GpuTaskSimple.java
+++ src/main/java/org/cloudbus/cloudsim/gp/cloudlets/gputasks/GpuTaskSimple.java
@@ -1,6 +1,39 @@
 package org.cloudbus.cloudsim.gp.cloudlets.gputasks;
 
+import org.cloudbus.cloudsim.utilizationmodels.UtilizationModel;
+import org.cloudbus.cloudsim.gp.cloudlets.GpuCloudletSimple;
+import org.cloudsimplus.listeners.CloudletVmEventInfo;
+import org.cloudsimplus.listeners.EventListener;
+
+import java.util.List;
+import java.util.Set;
+
 public class GpuTaskSimple implements GpuTask {
 	
-}
+    private long taskId;
+    private GpuCloudletSimple gpuCloudlet;
+    private long blockLength;
+    private long finishedLengthSoFar;
+    private long numberOfPes;
+    private Status status;
+    //private boolean returnedToBroker;
+    private double execStartTime;
+    //private int priority;
+    ///private int netServiceLevel;
+    private List<String> requiredFiles;
+    private long fileSize;
+    private long outputSize;
+    private double finishTime;
+    private UtilizationModel utilizationModelGpu;
+    private UtilizationModel utilizationModelGddram;
+    private UtilizationModel utilizationModelBw;
 
+    private final Set<EventListener<CloudletVmEventInfo>> onStartListeners;
+    private final Set<EventListener<CloudletVmEventInfo>> onFinishListeners;
+    private final Set<EventListener<CloudletVmEventInfo>> onUpdateProcessingListeners;
+
+    private double submissionDelay;
+    private double lifeTime;
+    //private double arrivalTime;
+    
+}
