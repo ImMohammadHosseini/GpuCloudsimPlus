2f4b53faba74ab516e7a48f505b213a00469b958
MohammadHosseini <sayedmh50@gmail.com>
2022-08-10 08:53:30 +0430

    replace GpuTask with GpuTaskSimple in gpu cloudlet

--- src/main/java/org/cloudbus/cloudsim/gp/cloudlets/GpuCloudletSimple.java
+++ src/main/java/org/cloudbus/cloudsim/gp/cloudlets/GpuCloudletSimple.java
@@ -3,43 +3,43 @@ package org.cloudbus.cloudsim.gp.cloudlets;
 import org.cloudbus.cloudsim.cloudlets.CloudletSimple;
 import org.cloudbus.cloudsim.utilizationmodels.UtilizationModel;
 
-import org.cloudbus.cloudsim.gp.cloudlets.gputasks.GpuTask;
+import org.cloudbus.cloudsim.gp.cloudlets.gputasks.GpuTaskSimple;
 
 import java.util.Objects;
 
 public class GpuCloudletSimple extends CloudletSimple {
 	
-	private GpuTask gpuTask;
+	private GpuTaskSimple gpuTask;
 	
 	public GpuCloudletSimple (final long length, final int pesNumber, 
-			final UtilizationModel utilizationModel, GpuTask gpuTask) {
+			final UtilizationModel utilizationModel, GpuTaskSimple gpuTask) {
 		super (length, pesNumber, utilizationModel);
 		setGpuTask (gpuTask);
 	}
 	
-	public GpuCloudletSimple (final long length, final int pesNumber, GpuTask gpuTask) {
+	public GpuCloudletSimple (final long length, final int pesNumber, GpuTaskSimple gpuTask) {
         super(length, pesNumber);
         setGpuTask (gpuTask);
 	}
 	
-	public GpuCloudletSimple (final long length, final long pesNumber, GpuTask gpuTask) {
+	public GpuCloudletSimple (final long length, final long pesNumber, GpuTaskSimple gpuTask) {
         super(length, pesNumber);
         setGpuTask (gpuTask);
     }
 	
 	public GpuCloudletSimple (final long id, final long length, final long pesNumber, 
-			GpuTask gpuTask) {
+			GpuTaskSimple gpuTask) {
         super(id, length, pesNumber);
         setGpuTask (gpuTask);
     }
 	
-	protected void setGpuTask (GpuTask gpuTask) {
+	protected void setGpuTask (GpuTaskSimple gpuTask) {
 		this.gpuTask = gpuTask;
 		if (gpuTask != null && gpuTask.getGpuCloudlet() == null)
 			gpuTask.setGpuCloudlet(this);
 	}
 	
-	public GpuTask getGpuTask () {
+	public GpuTaskSimple getGpuTask () {
 		return gpuTask;
 	}
 }
