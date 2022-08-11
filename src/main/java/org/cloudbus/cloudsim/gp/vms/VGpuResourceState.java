e4fc7f00ae0e54a4c8369224786a79892e5714bf
MohammadHosseini <sayedmh50@gmail.com>
2022-08-11 11:00:21 +0430

    add VGpuResourceState, it needs some change

---
+++ src/main/java/org/cloudbus/cloudsim/gp/vms/VGpuResourceState.java
@@ -0,0 +1,12 @@
+package org.cloudbus.cloudsim.gp.vms;
+
+import org.cloudbus.cloudsim.gp.resources.CustomVGpu;
+
+import org.cloudbus.cloudsim.vms.ResourceStats;
+
+public class VGpuResourceState extends ResourceStats<CustomVGpu> {
+	
+	public VGpuResourceState () {
+		
+	}
+}
