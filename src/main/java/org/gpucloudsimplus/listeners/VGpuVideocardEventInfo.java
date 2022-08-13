ad89551bf4d29962ffad3aa803168958fe253dbf
MohammadHosseini <sayedmh50@gmail.com>
2022-08-13 09:30:15 +0430

    add VGpuVideocardEventInfo

---
+++ src/main/java/org/gpucloudsimplus/listeners/VGpuVideocardEventInfo.java
@@ -0,0 +1,9 @@
+package org.gpucloudsimplus.listeners;
+
+import org.cloudsimplus.listeners.EventInfo;
+import org.cloudbus.cloudsim.gp.resources.CustomVGpu;
+import org.cloudbus.cloudsim.gp.resources.Gpu;
+
+public interface VGpuVideocardEventInfo extends VGpuEventInfo, VideocardEventInfo {
+	hhh
+}
