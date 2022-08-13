1b25904ba3887bd9bcc61c95140937a5d396771f
MohammadHosseini <sayedmh50@gmail.com>
2022-08-13 10:59:12 +0430

    add some methode in CustomVGpu interface

--- src/main/java/org/cloudbus/cloudsim/gp/resources/CustomVGpu.java
+++ src/main/java/org/cloudbus/cloudsim/gp/resources/CustomVGpu.java
@@ -3,9 +3,13 @@ package org.cloudbus.cloudsim.gp.resources;
 import org.cloudbus.cloudsim.gp.vms.CustomGpuVm;
 import org.cloudbus.cloudsim.gp.vms.CustomGpuVmNull;
 import org.cloudbus.cloudsim.gp.vms.CustomGpuVmSimple;
+import org.cloudbus.cloudsim.gp.videocards.Videocard;
 import org.cloudbus.cloudsim.resources.Ram;
+import org.cloudbus.cloudsim.resources.Resource;
 import org.cloudbus.cloudsim.resources.Bandwidth;
 import org.cloudbus.cloudsim.resources.Processor;
+import org.cloudsimplus.listeners.EventListener;
+import org.gpucloudsimplus.listeners.VGpuVideocardEventInfo;
 
 import java.util.List;
 
@@ -54,15 +58,57 @@ public interface CustomVGpu {
 	void setTenancy ();
 	
 	///
-	//String getDescription();
+	//String getDescription ();
 
-    //CustomVGpu setDescription(String description);
+    //CustomVGpu setDescription (String description);
 
-    //VmGroup getGroup();
+    //VmGroup getGroup ();
 
-    void addStateHistoryEntry(VGpuStateHistoryEntry entry);
+    void addStateHistoryEntry (VGpuStateHistoryEntry entry);
     
-    long getFreePesNumber();
+    long getFreePesNumber ();
 
-    long getExpectedFreePesNumber();
+    long getExpectedFreePesNumber ();
+    
+    CustomVGpu addOnVideocardAllocationListener (EventListener<VGpuVideocardEventInfo> listener);
+
+    CustomVGpu addOnMigrationStartListener (EventListener<VGpuVideocardEventInfo> listener);
+    
+    CustomVGpu addOnMigrationFinishListener (EventListener<VGpuVideocardEventInfo> listener);
+
+    CustomVGpu addOnHostDeallocationListener (EventListener<VGpuVideocardEventInfo> listener);
+    
+    //CustomVGpu addOnCreationFailureListener (EventListener<VmDatacenterEventInfo> listener);
+
+    CustomVGpu addOnUpdateProcessingListener (EventListener<VGpuVideocardEventInfo> listener);
+    
+    void notifyOnVideocardAllocationListeners ();
+
+    void notifyOnVideocardDeallocationListeners (Videocard deallocatedVideocard);
+    
+    //void notifyOnCreationFailureListeners (Datacenter failedDatacenter);
+
+    boolean removeOnMigrationStartListener (EventListener<VGpuVideocardEventInfo> listener);
+    
+    boolean removeOnMigrationFinishListener (EventListener<VGpuVideocardEventInfo> listener);
+    
+    boolean removeOnUpdateProcessingListener(EventListener<VGpuVideocardEventInfo> listener);
+    
+    boolean removeOnVideocardtAllocationListener (EventListener<VGpuVideocardEventInfo> listener);
+
+    boolean removeOnVideocardDeallocationListener(EventListener<VGpuVideocardEventInfo> listener);
+
+    //boolean removeOnCreationFailureListener(EventListener<VmDatacenterEventInfo> listener);
+
+    //@Override AbstractMachine
+    Resource getBw();
+
+    //@Override
+    Resource getRam();
+
+    //@Override
+    Resource getStorage();
+    
+    List<VGpuStateHistoryEntry> getStateHistory();
+
 }
