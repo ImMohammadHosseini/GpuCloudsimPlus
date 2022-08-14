17d707d430cee5b7e9d8a4c851d8a296190d68f1
MohammadHosseini <sayedmh50@gmail.com>
2022-08-14 12:19:14 +0430

    add methods to VideocardBwProvisioner interface

--- src/main/java/org/cloudbus/cloudsim/gp/provisioners/VideocardBwProvisioner.java
+++ src/main/java/org/cloudbus/cloudsim/gp/provisioners/VideocardBwProvisioner.java
@@ -1,5 +1,37 @@
 package org.cloudbus.cloudsim.gp.provisioners;
 
+import org.cloudbus.cloudsim.resources.ResourceManageable;
+import org.cloudbus.cloudsim.resources.Resource;
+import org.cloudbus.cloudsim.gp.resources.Gpu;
+
+import java.util.function.Function;
+
 public interface VideocardBwProvisioner {
 	
+	VideocardBwProvisioner NULL = new VideocardBwProvisionerNull();
+	
+	boolean allocateBwForGpu (Gpu gpu, long bw);
+	
+	default boolean allocateResourceForVm (final Gpu gpu, final double bw){
+        return allocateResourceForVm (gpu, (long)bw);
+    }
+	
+	long getAllocatedBwForGpu (Gpu gpu);
+	
+	long getTotalAllocatedBw ();
+	
+	long deallocateBwForGpu (Gpu gpu);
+	
+	boolean isSuitableForGpu (Gpu gpu, long newGpuTotalAllocatedBw);
+
+    boolean isSuitableForGpu (Gpu gpu, Resource resource);
+
+    ResourceManageable getvideocardBwResource ();
+
+    void setResources(ResourceManageable videocardBwResource, 
+    		Function<Gpu, ResourceManageable> gpuBwFunction);
+    
+    long getCapacity();
+
+    long getAvailableResource();
 }
--- src/main/java/org/cloudbus/cloudsim/gp/provisioners/VideocardBwProvisionerSimple.java
+++ src/main/java/org/cloudbus/cloudsim/gp/provisioners/VideocardBwProvisionerSimple.java
@@ -1,5 +1,16 @@
 package org.cloudbus.cloudsim.gp.provisioners;
 
+import org.cloudbus.cloudsim.resources.ResourceManageable;
+import org.cloudbus.cloudsim.gp.resources.Gpu;
+
+import java.util.function.Function;
+
 public class VideocardBwProvisionerSimple implements VideocardBwProvisioner {
 	
+	private ResourceManageable videocardBwResource;
+	
+	private Function<Gpu, ResourceManageable> gpuBwFunction;
+	
+	
+
 }
