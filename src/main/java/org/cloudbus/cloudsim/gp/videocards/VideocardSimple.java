400c560c132c8c59e779ae74522d7dcfe3fb2075
MohammadHosseini <sayedmh50@gmail.com>
2022-08-15 09:51:36 +0430

    add some methods to VideocardSimple

--- src/main/java/org/cloudbus/cloudsim/gp/videocards/VideocardSimple.java
+++ src/main/java/org/cloudbus/cloudsim/gp/videocards/VideocardSimple.java
@@ -1,5 +1,8 @@
 package org.cloudbus.cloudsim.gp.videocards;
 
+import org.cloudbus.cloudsim.gp.provisioners.VideocardBwProvisioner;
+import org.cloudbus.cloudsim.gp.schedulers.vgpu.VGpuScheduler;
+
 public class VideocardSimple implements Videocard {
 	
 	private long id;
@@ -11,5 +14,43 @@ public class VideocardSimple implements Videocard {
 		
 	}
 	
+	@Override 
+	public long getId () {
+		return id;
+	}
+	
+	@Override 
+	public void setId (long id) {
+		this.id = id;
+	}
+	
+	@Override 
+	public String getType () {
+		return type;
+	}
+	
+	@Override 
+	public void setType (String type) {
+		this.type = type;
+	}
+	
+	@Override 
+	public VGpuScheduler getVGpuScheduler () {
+		return vgpuScheduler;
+	}
+	
+	@Override 
+	public Videocard setVGpuScheduler (VGpuScheduler vgpuScheduler) {
+		
+	}
+	
+	@Override 
+	public VideocardBwProvisioner getPcieBwProvisioner () {
+		return pcieBwProvisioner;
+	}
+	
+	@Override public Videocard setPcieBwProvisioner (VideocardBwProvisioner pcieBwProvisioner) {
+		
+	}
 }
 
