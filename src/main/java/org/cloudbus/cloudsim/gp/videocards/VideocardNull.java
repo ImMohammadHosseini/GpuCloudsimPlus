537a966ffe09b08e067a216a8ca4db18d91404ea
MohammadHosseini <sayedmh50@gmail.com>
2022-08-15 09:37:07 +0430

    write videocardNull copletely

--- src/main/java/org/cloudbus/cloudsim/gp/videocards/VideocardNull.java
+++ src/main/java/org/cloudbus/cloudsim/gp/videocards/VideocardNull.java
@@ -1,6 +1,34 @@
 package org.cloudbus.cloudsim.gp.videocards;
 
+import org.cloudbus.cloudsim.gp.provisioners.VideocardBwProvisioner;
+import org.cloudbus.cloudsim.gp.schedulers.vgpu.VGpuScheduler;
+
 public class VideocardNull implements Videocard {
+
+	@Override public long getId () {
+		return -1;
+	}
 	
+	@Override public void setId (long id) {/**/}
+	
+	@Override public String getType () { return "";}
+	
+	@Override public void setType (String type) {/**/}
+	
+	@Override public VGpuScheduler getVGpuScheduler () {
+		return VGpuScheduler.NULL;
+	}
+	
+	@Override public Videocard setVGpuScheduler (VGpuScheduler vgpuScheduler) {
+		return this;
+	}
+	
+	@Override public VideocardBwProvisioner getPcieBwProvisioner () {
+		return VideocardBwProvisioner.NULL;
+	}
+	
+	@Override public Videocard setPcieBwProvisioner (VideocardBwProvisioner pcieBwProvisioner) {
+		return this;
+	}
 }
 
