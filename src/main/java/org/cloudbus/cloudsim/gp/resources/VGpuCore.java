8ddfadd1f5689b0d342720d71e47418e18487b48
MohammadHosseini <sayedmh50@gmail.com>
2022-08-16 11:25:38 +0430

    change name: VGpuProcessor to VGpuCore

---
+++ src/main/java/org/cloudbus/cloudsim/gp/resources/VGpuCore.java
@@ -0,0 +1,12 @@
+package org.cloudbus.cloudsim.gp.resources;
+
+import org.cloudbus.cloudsim.resources.ResourceManageableAbstract;
+
+public final class VGpuCore extends ResourceManageableAbstract {
+	public static final VGpuCore NULL = new VGpuCore(0);
+	
+	public VGpuCore () {
+		
+	}
+}
+    
