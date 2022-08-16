e2be83e96403f640e35cc2481ace46b5a5a95838
MohammadHosseini <sayedmh50@gmail.com>
2022-08-16 11:42:04 +0430

    write VGpuCore Completely

--- src/main/java/org/cloudbus/cloudsim/gp/resources/VGpuCore.java
+++ src/main/java/org/cloudbus/cloudsim/gp/resources/VGpuCore.java
@@ -5,8 +5,61 @@ import org.cloudbus.cloudsim.resources.ResourceManageableAbstract;
 public final class VGpuCore extends ResourceManageableAbstract {
 	public static final VGpuCore NULL = new VGpuCore(0);
 	
-	public VGpuCore () {
-		
+	private CustomVGpu vgpu;
+	private double mips;
+	
+	public VGpuCore (final CustomVGpu vgpu, final double mips, final long numberOfPes) {
+		this(numberOfPes);
+        this.vgpu = vgpu;
+        setMips(mips);
 	}
+	
+	private VGpuCore (final long numberOfPes){
+		super(numberOfPes, "Unit");
+    }
+	
+	public void setMips(final double newMips) {
+        if(newMips < 0) {
+            throw new IllegalArgumentException("MIPS cannot be negative");
+        }
+
+        this.mips = newMips;
+    }
+	
+	public double getMips() {
+        return mips;
+    }
+
+	public double getTotalMips(){
+        return getMips()* getCapacity();
+    }
+	
+	//getNumberOfPes
+	@Override
+    public long getCapacity() {
+        return super.getCapacity();
+    }
+	
+	@Override
+    public long getAvailableResource() {
+        return super.getAvailableResource();
+    }
+
+    @Override
+    public long getAllocatedResource() {
+        return super.getAllocatedResource();
+    }
+    
+    @Override
+    public boolean setCapacity(long numberOfPes) {
+        if(numberOfPes <= 0){
+            throw new IllegalArgumentException("The Processor's number of PEs must be greater than 0.");
+        }
+        return super.setCapacity(numberOfPes);
+    }
+
+    public CustomVGpu getVgpu() {
+        return vgpu;
+    }
 }
     
