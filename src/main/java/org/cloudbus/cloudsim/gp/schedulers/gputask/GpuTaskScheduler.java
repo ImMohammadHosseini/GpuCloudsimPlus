a283211c850b1e195d5d1c460307f6ce764d4f3c
MohammadHosseini <sayedmh50@gmail.com>
2022-08-19 17:44:23 +0430

    add feature in GpuTaskScheduler

--- src/main/java/org/cloudbus/cloudsim/gp/schedulers/gputask/GpuTaskScheduler.java
+++ src/main/java/org/cloudbus/cloudsim/gp/schedulers/gputask/GpuTaskScheduler.java
@@ -2,8 +2,15 @@ package org.cloudbus.cloudsim.gp.schedulers.gputask;
 
 import java.io.Serializable;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 public interface GpuTaskScheduler extends Serializable {
+	Logger LOGGER = LoggerFactory.getLogger(GpuTaskScheduler.class.getSimpleName());
+	
 	GpuTaskScheduler NULL = new GpuTaskSchedulerNull();
+	
+	
 }
 
--- src/main/java/org/cloudbus/cloudsim/gp/schedulers/gputask/GpuTaskSchedulerAbstract.java
+++ src/main/java/org/cloudbus/cloudsim/gp/schedulers/gputask/GpuTaskSchedulerAbstract.java
@@ -1,6 +1,44 @@
 package org.cloudbus.cloudsim.gp.schedulers.gputask;
 
+import org.cloudbus.cloudsim.gp.cloudlets.gputasks.GpuTask;
+import org.cloudbus.cloudsim.gp.resources.CustomVGpu;
+
+import org.cloudbus.cloudsim.schedulers.MipsShare;
+import java.util.*;
+
 
 public abstract class GpuTaskSchedulerAbstract implements GpuTaskScheduler {
 	
+	private final List<CloudletExecution> gpuTaskPausedList;
+    private final List<CloudletExecution> gpuTaskFinishedList;
+    private final List<CloudletExecution> gpuTaskFailedList;
+    private final List<CloudletExecution> gpuTaskExecList;
+    private final List<CloudletExecution> gpuTaskWaitingList;
+    
+    private final List<GpuTask> gpuTaskSubmittedList;
+    private final Set<GpuTask> gpuTaskReturnedList;
+    
+    //private CloudletTaskScheduler taskScheduler;
+    private double previousTime;
+    private MipsShare currentMipsShare;
+    private boolean enableGpuTastSubmittedList;
+    
+    private CustomVGpu vgpu;
+    
+    private final List<EventListener<CloudletResourceAllocationFailEventInfo>> resourceAllocationFailListeners;
+
+    protected GpuTaskSchedulerAbstract() {
+        setPreviousTime(0.0);
+        vgpu = CustomVGpu.NULL;
+        gpuTaskSubmittedList = new ArrayList<>();
+        gpuTaskExecList = new ArrayList<>();
+        gpuTaskPausedList = new ArrayList<>();
+        gpuTaskFinishedList = new ArrayList<>();
+        gpuTaskFailedList = new ArrayList<>();
+        gpuTaskWaitingList = new ArrayList<>();
+        gpuTaskReturnedList = new HashSet<>();
+        currentMipsShare = new MipsShare();
+        //taskScheduler = CloudletTaskScheduler.NULL;
+        resourceAllocationFailListeners = new ArrayList<>();
+    }
 }
