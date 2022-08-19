80dfce89f55689ddee52815e2b402ff0e7d7c145
MohammadHosseini <sayedmh50@gmail.com>
2022-08-19 16:00:24 +0430

    some changes in GpuCloudletScheduler

--- src/main/java/org/cloudbus/cloudsim/gp/schedulers/gpucloudlet/GpuCloudletScheduler.java
+++ src/main/java/org/cloudbus/cloudsim/gp/schedulers/gpucloudlet/GpuCloudletScheduler.java
@@ -1,9 +1,19 @@
 package org.cloudbus.cloudsim.gp.schedulers.gpucloudlet;
 
+import org.cloudbus.cloudsim.schedulers.cloudlet.CloudletScheduler;
+import org.cloudbus.cloudsim.cloudlets.CloudletExecution;
+
+import java.util.*;
+import org.slf4j.Logger;
 import java.io.Serializable;
+import org.slf4j.LoggerFactory;
 
 
-public interface GpuCloudletScheduler extends Serializable {
+public interface GpuCloudletScheduler extends Serializable, CloudletScheduler {
+	Logger LOGGER = LoggerFactory.getLogger(GpuCloudletScheduler.class.getSimpleName());
+
 	GpuCloudletScheduler NULL = new GpuCloudletSchedulerNull();
+	
+	
 }
 
