66383cc9cf9ec688d8a47e843f570d468edbeb98
MohammadHosseini <sayedmh50@gmail.com>
2022-08-23 09:55:00 +0430

    add GpuTaskVGpuEventInfo

---
+++ src/main/java/org/gpucloudsimplus/listeners/GpuTaskVGpuEventInfo.java
@@ -0,0 +1,36 @@
+package org.gpucloudsimplus.listeners;
+
+import org.cloudbus.cloudsim.gp.cloudlets.gputasks.GpuTask;
+import org.cloudbus.cloudsim.gp.resources.CustomVGpu;
+
+import org.cloudsimplus.listeners.EventListener;
+import org.cloudsimplus.listeners.EventInfo;
+
+public interface GpuTaskVGpuEventInfo extends GpuTaskEventInfo, VGpuEventInfo {
+	
+	static GpuTaskVGpuEventInfo of (final EventListener<? extends EventInfo> listener, 
+			final GpuTask gpuTask, final CustomVGpu vgpu) {
+        return of(listener, gpuTask.getSimulation().clock(), gpuTask, vgpu);
+    }
+
+    static GpuTaskVGpuEventInfo of (final EventListener<? extends EventInfo> listener, 
+    		final double time, final GpuTask gpuTask) {
+        return of(listener, time, gpuTask, gpuTask.getVGpu());
+    }
+    
+    static GpuTaskVGpuEventInfo of (final EventListener<? extends EventInfo> listener, 
+    		final GpuTask gpuTask) {
+        return of(listener, gpuTask, gpuTask.getVGpu());
+    }
+
+    static GpuTaskVGpuEventInfo of (final EventListener<? extends EventInfo> listener, 
+    		final double time, final GpuTask gpuTask, final CustomVGpu vgpu){
+        return new GpuTaskVGpuEventInfo () {
+        	@Override public GpuTask getGpuTask () { return gpuTask; }
+            @Override public CustomVGpu getVGpu () { return vgpu; }
+            @Override public double getTime () { return time; }
+            @Override public EventListener<? extends EventInfo> getListener () { return listener; }
+        };
+    }
+
+}
