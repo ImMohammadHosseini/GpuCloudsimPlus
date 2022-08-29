296c34a25a053f5a34b73aa59928de7a9b7de58b
MohammadHosseini <sayedmh50@gmail.com>
2022-08-29 15:00:58 +0430

    add VGpuAllocationPolicySimple methods

--- src/main/java/org/cloudbus/cloudsim/gp/allocationpolicies/VGpuAllocationPolicySimple.java
+++ src/main/java/org/cloudbus/cloudsim/gp/allocationpolicies/VGpuAllocationPolicySimple.java
@@ -1,7 +1,34 @@
 package org.cloudbus.cloudsim.gp.allocationpolicies;
 
+import java.util.stream.*;
+import java.util.Comparator;
+import java.util.Optional;
+import java.util.function.BiFunction;
+
+import org.cloudbus.cloudsim.gp.resources.CustomVGpu;
+import org.cloudbus.cloudsim.gp.resources.Gpu;
+
+import static java.util.Comparator.comparing;
 
 public  class VGpuAllocationPolicySimple extends VGpuAllocationPolicyAbstract {
 
+	public VGpuAllocationPolicySimple () {
+        super();
+    }
+
+    public VGpuAllocationPolicySimple (
+    		final BiFunction<VGpuAllocationPolicy, CustomVGpu, Optional<Gpu>> findGpuForVGpuFunction) {
+        super(findGpuForVGpuFunction);
+    }
+    
+    @Override
+    protected Optional<Gpu> defaultFindGpuForVGpu (final CustomVGpu vgpu) {
+        final Comparator<Gpu> comparator = comparing(Gpu::isActive).thenComparingLong(
+        		Gpu::getFreeCoresNumber);
+
+        final Stream<Gpu> gpuStream = isParallelGpuSearchEnabled() ? getGpuList().stream().parallel() : 
+        	getGpuList().stream();
+        return gpuStream.filter(gpu -> gpu.isSuitableForVGpu(vgpu)).max(comparator);
+    }
 }
 
