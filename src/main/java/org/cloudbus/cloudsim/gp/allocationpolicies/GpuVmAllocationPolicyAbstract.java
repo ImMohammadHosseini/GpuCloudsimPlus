486d320a0243a2185721377d198ad6bb2524b25c
MohammadHosseini <sayedmh50@gmail.com>
2022-09-03 09:30:29 +0430

    change name CustomVmAllocationPolicy to GpuVm...

---
+++ src/main/java/org/cloudbus/cloudsim/gp/allocationpolicies/GpuVmAllocationPolicyAbstract.java
@@ -0,0 +1,11 @@
+package org.cloudbus.cloudsim.gp.allocationpolicies;
+
+import org.cloudbus.cloudsim.allocationpolicies.VmAllocationPolicyAbstract;
+
+public abstract class GpuVmAllocationPolicyAbstract extends VmAllocationPolicyAbstract{
+	
+	public GpuVmAllocationPolicyAbstract () {
+		
+	}
+	
+}
---
+++ src/main/java/org/cloudbus/cloudsim/gp/allocationpolicies/GpuVmAllocationPolicySimple.java
@@ -0,0 +1,11 @@
+package org.cloudbus.cloudsim.gp.allocationpolicies;
+
+
+public abstract class GpuVmAllocationPolicySimple extends GpuVmAllocationPolicyAbstract{
+	
+	public GpuVmAllocationPolicySimple () {
+		
+	}
+	
+}
+
